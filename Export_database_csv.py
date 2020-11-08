23cb9163103bdcf9ec0aedd3c5360d26cbbcbd7c
Guigxs <guigxs19@gmail.com>
2020-11-08 15:30:47 +0100

    Export to json

--- Export_database_csv.py
+++ Export_database_csv.py
@@ -1,18 +1,28 @@
 import sqlite3
+import json
 
 tables = ["orders_order", "coupons_coupon", "orders_orderitem", "shop_product", "shop_category", "auth_user"]
 
-with open('test.csv', 'w+') as write_file:
-    conn = sqlite3.connect('db.sqlite3') # connect to your database 
-    cursor = conn.cursor() # create a cursor object (which lets you address the table results individually) 
-    for table in tables:
-        write_file.write(table)
-        for row in cursor.execute(f'SELECT * FROM {table}'): # use the cursor as an iterable 
-            datas = ""
-            for i in row:
-                datas += f'{i},'
+conn = sqlite3.connect('db.sqlite3') # connect to your database 
+cursor = conn.cursor() # create a cursor object (which lets you address the table results individually) 
+jsonTables = {}
+for i in range(len(tables)):
+    liste = []
+    for row in cursor.execute(f'SELECT * FROM {tables[i]}'): # use the cursor as an iterable 
+        liste.append(list(row))
+    fields = [a[0] for a in cursor.description]
 
-            write_file.write(";")
-            write_file.write(datas) # write to the csv, then you can open the csv in Excel. 
-        write_file.write("\n")
+    objetListe = []
+    for j in range(len(liste)):
+        objet = {}
+        for k in range(len(fields)):
+            objet[fields[k]] = liste[j][k]
+        
+        objetListe.append(objet)
+        
+    jsonTables[tables[i]] = objetListe
+
+with open('datas.json', 'w') as write_file:
+    json.dump(jsonTables, write_file)
+
 
